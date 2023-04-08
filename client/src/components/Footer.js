968da81c5d2dee1b05d818b9c0ab6217dd0d044b
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-04-08 16:55:19 +0500

    [ADD] Router ProductScreen

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <Container>
                 <Row>
-                    <Col className='text-center py-3'>Copyright &copy; WaleedShop</Col>
+                    <Col className='text-center py-3'>Copyright &copy; ShopNow</Col>
                 </Row>
             </Container>
         </footer>

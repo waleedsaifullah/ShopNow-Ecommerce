24753662420c4fa373366ec27cd3680805a8e5e9
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-04-21 02:43:42 +0500

    Backend server added

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+const products = require('./data/products')
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running....')
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+});
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`))
+

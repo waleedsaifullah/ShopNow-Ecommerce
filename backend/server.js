aaa35c5561909c6999138887765fff02565f6a9f
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-04-25 10:14:19 +0500

    error middleware added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv').config();
 const colors = require('colors')
 
 const connectDB = require('./config/db')
-const products = require('./data/products')
+const productRoutes = require('./routes/productRoutes')
+const { notFound, errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -14,14 +15,10 @@ app.get('/', (req, res) => {
     res.send('API is running....')
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.green.bold))
 
